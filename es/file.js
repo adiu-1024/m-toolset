40972f8f82cbebdf65bb27f84ad563b01ac3668b
adiu <adiu_1024@163.com>
2021-03-29 21:30:35 +0800

    Code optimization

--- es/file.js
+++ es/file.js
@@ -7,22 +7,22 @@ const getVideoInfo = function(file, captureImage = true) {
     })
     if (captureImage) {
       video.addEventListener('loadeddata', function() {
-        const { videoWidth, videoHeight, duration } = this
+        const { videoWidth: width, videoHeight: height, duration } = this
         const canvas = document.createElement('canvas')
-        Object.assign(canvas, {
-          width: videoWidth, height: videoHeight
-        })
+        Object.assign(canvas, { width, height })
         canvas.getContext('2d').drawImage(this, 0, 0, canvas.width, canvas.height)
         canvas.toBlob(blob => {
           URL.revokeObjectURL(this.src)
-          resolve({ blob, videoWidth, videoHeight, duration })
+          this.src = ''
+          resolve({ blob, width, height, duration })
         }, 'image/jpeg', 0.95)
       })
     } else {
       video.addEventListener('loadedmetadata', function() {
-        const { videoWidth, videoHeight, duration } = this
+        const { videoWidth: width, videoHeight: height, duration } = this
         URL.revokeObjectURL(this.src)
-        resolve({ videoWidth, videoHeight, duration })
+        this.src = ''
+        resolve({ width, height, duration })
       })
     }
     video.onerror = (error) => { reject(error) }
